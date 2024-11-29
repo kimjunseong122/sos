a3c549ff52578f91c9a294762ec621358138530e
kimjunseong122 <148169946+kimjunseong122@users.noreply.github.com>
2024-11-29 15:06:23 +0900

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,15 @@
+x = 1; y = 2
+str1 = 'abc';str2 = 'python'
+print(x == y)
+print(x != y)
+print(str1 == str2)
+print(str2 == 'python')
+print(str1 < str2)
+
+
+#A == B -> A와 B가 같으면 참
+#A != B -> A와 B가 다르면 참
+#A < B  -> A가 B보다 작으면 참
+#A <= B -> A가 B보다 작거나 같으면 참
+#A > B  -> A가 B보다 크면 참
+#A >= B -> A가 B보다 크거나 같으면 참
